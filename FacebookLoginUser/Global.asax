5620aa27eb21bc56769b57b2c60e39f5a082232a
tehsaya <toanlmse03407@fpt.edu.vn>
2016-05-09 00:26:08 +0700

    First commit

---
+++ FacebookLoginUser/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FacebookLoginUser.MvcApplication" Language="C#" %>
---
+++ FacebookLoginUser/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FacebookLoginUser.Startup))]
+namespace FacebookLoginUser
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
